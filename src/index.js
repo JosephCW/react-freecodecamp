db79524631993ed4574a548940eb947515ab1447
Joseph Watts <josephcw@live.com>
2021-02-20 16:27:30 -0600

    Complete Functional Components Practice

--- src/index.js
+++ src/index.js
@@ -11,21 +11,51 @@ import reportWebVitals from './reportWebVitals';
 //   document.getElementById('root')
 // );
 
+// ReactDOM & JSX Practice
 // components in pascal case
-function MyApp() {
+// function MyApp() {
+//   return (
+//     <ul>
+//       <li>a</li>
+//       <li>b</li>
+//       <li>c</li>
+//     </ul>
+//   )
+// }
+
+// ReactDOM.render(
+//   <MyApp />,
+//   document.getElementById("root")
+// );
+
+
+// React Functional Components Practice
+// Objectives:
+// 1. Set up the basic React code from scratch
+// 2. Create a functional component called MyInfo that returns the following UI:
+    // a. An h1 with your name
+    // b. A paragraph with a little blurb about yourself
+    // c. An ordered or unordered list of the top 3 vacation spots you'd like to visit
+// 3. Render an instance of that functional component to the browser
+
+function MyInfo() {
   return (
-    <ul>
-      <li>a</li>
-      <li>b</li>
-      <li>c</li>
-    </ul>
+    <div>
+      <h1>Joseph Watts</h1>
+      <p>I am a 23yo software engineer. I have some experience with express but never with React</p>
+      <ol>
+        <li>Steamboat Springs, CO</li>
+        <li>Telluride, CO</li>
+        <li>Vail, CO</li>
+      </ol>
+    </div>
   )
 }
 
 ReactDOM.render(
-  <MyApp />,
+  <MyInfo />,
   document.getElementById("root")
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
