8295baaa6b69b1a2b6533b945d5cd7151b8dba61
Joseph Watts <josephcw@live.com>
2021-02-21 15:38:24 -0600

    Checklist items are now pulled from state

--- src/components/App.js
+++ src/components/App.js
@@ -4,46 +4,65 @@ import ChecklistItem from "./ChecklistItem"
 import Joke from "./Joke"
 import checklistData from "../data/checklistData"
 
-function App() {
-    // return (
-    //     <div className="checklist">
-    //         <ChecklistItem />
-    //         <ChecklistItem />
-    //         <ChecklistItem />
-    //         <ChecklistItem />
-    //     </div>
-    // )
+// function App() {
+//     // return (
+//     //     <div className="checklist">
+//     //         <ChecklistItem />
+//     //         <ChecklistItem />
+//     //         <ChecklistItem />
+//     //         <ChecklistItem />
+//     //     </div>
+//     // )
 
-    // return (
-    //     <div className="joke-list">
-    //         <Joke 
-    //             question = "What's green and has wheels?"
-    //             punchLine = "Grass! I lied about the wheels."
-    //         />
-    //         <Joke 
-    //             question = "Why do scuba divers jump backwards out of the boat?"
-    //             punchLine = "Because if they jumped forward, they’d still be in the boat."
-    //         />
-    //         <Joke 
-    //             question = "What do you call a broken can opener?"
-    //             punchLine = "A can’t opener."
-    //         />
-    //         <Joke 
-    //             question = "What’s the best thing about Switzerland?"
-    //             punchLine = "Well the flag’s a big plus."
-    //         />
-    //         <Joke 
-    //             punchLine = "Be wary of stairs…they’re almost always up to something."
-    //         />
-    //     </div>
-    // )
+//     // return (
+//     //     <div className="joke-list">
+//     //         <Joke 
+//     //             question = "What's green and has wheels?"
+//     //             punchLine = "Grass! I lied about the wheels."
+//     //         />
+//     //         <Joke 
+//     //             question = "Why do scuba divers jump backwards out of the boat?"
+//     //             punchLine = "Because if they jumped forward, they’d still be in the boat."
+//     //         />
+//     //         <Joke 
+//     //             question = "What do you call a broken can opener?"
+//     //             punchLine = "A can’t opener."
+//     //         />
+//     //         <Joke 
+//     //             question = "What’s the best thing about Switzerland?"
+//     //             punchLine = "Well the flag’s a big plus."
+//     //         />
+//     //         <Joke 
+//     //             punchLine = "Be wary of stairs…they’re almost always up to something."
+//     //         />
+//     //     </div>
+//     // )
 
-    return (
-        <div className="checklist">
-            {checklistData.map((item) => <ChecklistItem key={item.id} item={item} />)}
-        </div>
-    )
+//     return (
+//         <div className="checklist">
+//             {checklistData.map((item) => <ChecklistItem key={item.id} item={item} />)}
+//         </div>
+//     )
 
+// }
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            items: checklistData
+        }
+    }
+
+    render() {
+        const checklistItems = this.state.items.map((item) => <ChecklistItem key={item.id} item={item} />)
+
+        return (
+            <div className="checklist">
+                {checklistItems}
+            </div>
+        )
+    }
 }
 
 export default App
