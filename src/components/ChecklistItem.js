b5fd09e2caf3eeec7f3afcf99d2ac3f5ea97acd3
Joseph Watts <josephcw@live.com>
2021-02-21 16:09:43 -0600

    Handle events

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -1,10 +1,18 @@
 import React from "react"
 
+function onCheckChanged() {
+    console.log('abc')
+}
+
 function ChecklistItem(props) {
 
     return (
         <div className="checklist-item">
-            <input type="checkbox" checked={props.item.completed}/>
+            <input 
+                type="checkbox" 
+                onChange={onCheckChanged} 
+                checked={props.item.completed}
+            />
             <span>{props.item.text}</span>
         </div>
     )
